ace59c9c7e8c59eaa31406b394b7314de86fe882
Falin-dev <mounsandofalin.ai23@stellamaryscoe.edu.in>
2026-08-24 11:04:33 +0530

    Completed Login Function Completely

--- penguin/src/components/Login/SignIn/index.jsx
+++ penguin/src/components/Login/SignIn/index.jsx
@@ -5,15 +5,13 @@ class SignIn extends Component {
     state = { username: "", password: "", prompt: "" }
 
     triggerDisplaySignUp = (displaySignUp) => {
-        console.log("entered")
         displaySignUp();
     }
 
-    triggerSignIn = async (e,username,password,loginUser) => {
+    triggerSignIn = async (e, username, password, loginUser) => {
         e.preventDefault();
-        console.log("asfi")
         const response = await loginUser(username, password)
-        this.setState(()=>({username:"",password:"",prompt:response}))
+        this.setState(() => ({ username: "", password: "", prompt: response }))
     }
 
 
@@ -28,20 +26,21 @@ class SignIn extends Component {
 
     render() {
         const { displaySignUp, loginUser } = this.props
-        const {username,password, prompt} = this.state
+        const { username, password, prompt } = this.state
+
         return (
-            <form className="login-main" onSubmit={e => this.triggerSignIn(e,loginUser, username, password)} action="submit">
+            <form className="login-main" onSubmit={e => this.triggerSignIn(e, username, password, loginUser)} action="submit">
                 <h1>Login</h1>
                 <div className="form">
                     <label htmlFor="username">Username</label>
-                    <input name="username" id="username" placeholder="falin-dev" />
+                    <input onChange={this.handleFields} name="username" id="username" placeholder="falin-dev" />
                     <br />
                     <label htmlFor="username">Password</label>
-                    <input name="password" id="password" placeholder="falin#*&100" />
+                    <input onChange={this.handleFields} name="password" id="password" placeholder="falin#*&100" />
                 </div>
-                {prompt&&<p className="prompt">{prompt}</p>}
+                {prompt && <p className="prompt">{prompt}</p>}
                 <div className="button-pair">
-                    <button onClick={e => this.triggerSignIn(username,password,loginUser)}>Login</button>
+                    <button onClick={e => this.triggerSignIn(e, username, password, loginUser)}>Login</button>
                     <button onClick={e => this.triggerDisplaySignUp(displaySignUp)} ><p>Create New Account</p></button>
                 </div>
             </form>
--- penguin/src/components/Login/index.jsx
+++ penguin/src/components/Login/index.jsx
@@ -3,36 +3,47 @@ import SignUp from "./SignUp/";
 import SignIn from "./SignIn/"
 import "./index.css"
 import { v4 as uuidv4 } from 'uuid';
-const userDetails = [
-    {
-        id:uuidv4(),
-        username:"Falin-dev",
-        password:"falin"
-    }
-]
+
 const backendDetails = {
     url:"http://localhost:3000/",
+    login:"login/",
+    addUser:"add-user/"
 
 }
 class Login extends Component {
     state = { loginPage: true }
     
     loginUser = async (username,password)=>{
-        return "hello";
+        const loginUserDetails = JSON.stringify({
+            username:username,
+            password:password
+        })
+        const {url,login} = backendDetails
+        const loginEndPoint = url+login;
+        const loginUserRequest = await fetch(loginEndPoint, {
+            method: "POST",
+            headers: {
+                "Content-Type":"application/json"
+            },
+            body:loginUserDetails
+        })
+        const responseData = await loginUserRequest.json();
+        const responseMessage = responseData.response;
+        return responseMessage;
     }
 
     addUser = async (username,password)=>{
-        const newUserDetails = {
+        const newUserDetails = JSON.stringify({
             username:username,
             password:password
-        }
+        });
         const addUserEndPoint = backendDetails.url+"add-user/"
         const addUserRequest = await fetch(addUserEndPoint,{
             method: "POST",
             headers: {
                 "Content-Type" : "application/json",
             },
-            body: JSON.stringify(newUserDetails)
+            body: newUserDetails
         })
         const responseData = await addUserRequest.json();
         const responseMessage = responseData.response;
@@ -40,7 +51,7 @@ class Login extends Component {
     }
 
     loginuser = async (username,password)=>{
-
+        return "Hi User"
     }
 
 
@@ -53,7 +64,7 @@ class Login extends Component {
         const {loginPage} = this.state
         console.log(loginPage)
         return (
-            loginPage?<SignIn logInUser={this.loginUser} displaySignUp={this.displaySignUp}/>:<SignUp addUser={this.addUser} displaySignUp={this.displaySignUp}/>
+            loginPage?<SignIn loginUser={this.loginUser} displaySignUp={this.displaySignUp}/>:<SignUp addUser={this.addUser} displaySignUp={this.displaySignUp}/>
         )
     }
 }
