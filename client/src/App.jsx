61f5fb134e5e25a300d86ffa118a4097bb11c834
Falin-dev <mounsandofalin.ai23@stellamaryscoe.edu.in>
2026-09-21 15:51:17 +0530

    minor changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./features/auth/pages/LoginPage";
 import RegisterPage from "./features/auth/pages/RegisterPage";
 import FeedPage from "./features/posts/pages/FeedPage";
+import Navbar from "./components/Navbar";
 function App() {
     return (
         <BrowserRouter>
+        <Navbar/>
             <Routes>
-                <Route exact path="/" element={<p>Hi</p>}/>
+                <Route exact path="/" element={<LoginPage />} />
+                
                 <Route exact path="/login" element={<LoginPage />} />
                 <Route exact path="/register" element={<RegisterPage />} />
                 <Route exact path="/feed" element={<FeedPage />}/>
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,1 +1,12 @@
-import {useState} from "react"
+import {Link} from "react-router-dom"
+const Navbar = ()=>{
+    return(
+        <nav>
+            <Link to="/" >Penguin</Link>
+            <Link to="/login" >Login</Link>
+            <Link to="/register" >Register</Link>
+        </nav>
+    )
+}
+
+export default Navbar
